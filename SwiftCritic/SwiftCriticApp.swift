5fca42e3bd51bc794cc7e7d49c7794e5a8c25ed8
KazumaKun1 <arviejhay123@gmail.com>
2026-06-22 22:30:47 +0800

    Initial Commit

---
+++ SwiftCritic/SwiftCriticApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftCriticApp.swift
+//  SwiftCritic
+//
+//  Created by Arviejhay Alejandro on 6/22/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftCriticApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SwiftCriticTests/SwiftCriticTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftCriticTests.swift
+//  SwiftCriticTests
+//
+//  Created by Arviejhay Alejandro on 6/22/26.
+//
+
+import Testing
+@testable import SwiftCritic
+
+struct SwiftCriticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
